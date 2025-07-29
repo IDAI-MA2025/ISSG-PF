1588872666d6b07677d64eff5c2abb17918d079a
ItanSegovia <al44653@un.edu.mx>
2025-07-29 00:23:36 -0600

    subi lo que tenia que subir

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
-var btnMenuOpen = document.getElementById('btnMenuOpen')
-var btnMenuClose = document.getElementById('btnMenuClose')
-var menubar = document.getElementById('menubar')
+var btnMenuOpen = document.getElementById('btnMenuOpen');
+var btnMenuClose = document.getElementById('btnMenuClose');
+var menubar = document.getElementById('menubar');
 
-btnMenuOpen.addEventListener('click', function(){
-    menubar.classList.add(active);
+btnMenuOpen.addEventListener('click', function() {
+    menubar.classList.add('active');
 });
 
-btnMenuClose.addEventListener('click', function(){
-    menubar.classList.remove(active);
+btnMenuClose.addEventListener('click', function() {
+    menubar.classList.remove('active');
 });
 
 var enlaces = document.getElementById('enlaces');
 enlaces.addEventListener('click', function() {
     menubar.style.transitionDelay = '0.5s';
     menubar.classList.remove('active');
+});
+
+var contenedor 
+    = document.querySelector('.slider');
+var btnIzq = document.getElementById("btn-izquierda");
+var btnDer = document.getElementById("btn-derecha");
+
+btnDer.addEventListener('click', function() {
+    contenedor.scrollLeft += contenedor.offsetWidth;
+});
+
+btnIzq.addEventListener('click', function() {
+    contenedor.scrollLeft -= contenedor.offsetWidth;
 });
